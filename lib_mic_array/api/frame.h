0584559ac55f5c2e20b93ae116901f6da6d21bfe
andrewstanfordjason <andrew@xmos.com>
2015-11-24 16:34:00 +0000

    changed type of frames to int32_t

--- lib_mic_array/api/frame.h
+++ lib_mic_array/api/frame.h
@@ -3,32 +3,33 @@
 #define PCM_FRAME_H_
 
 #define MAX_NUM_MICS 8
+#include <stdint.h>
 
 #include "mic_array_conf.h"
 
 //This must have an even number of words
 typedef struct {
-    int min;                /**<The minimum data value in this frame. UNUSED*/
-    int max;                /**<The maximum data value in this frame. */
+    int32_t min;                /**<The minimum data value in this frame. UNUSED*/
+    int32_t max;                /**<The maximum data value in this frame. */
     unsigned frame_number;  /**<The frame_number. UNUSED*/
     unsigned x;  /**<The frame_number. UNUSED*/
 } s_metadata;
 
 /** Complex number in Cartesian coordinates.*/
 typedef struct {
-    int re;     /**<The real component. */
-    int im;     /**<The imaginary component. */
+    int32_t re;     /**<The real component. */
+    int32_t im;     /**<The imaginary component. */
 } complex;
 
 /** Complex number in Polar coordinates.*/
 typedef struct {
-    int hyp;        /**<The hypotenuse component. */
-    int theta;      /**<The angle component. */
+    int32_t hyp;        /**<The hypotenuse component. */
+    int32_t theta;      /**<The angle component. */
 } polar;
 
 /** A frame of raw audio.*/
 typedef struct {
-    int data[MAX_NUM_MICS][1<<FRAME_SIZE_LOG2];/**< Raw audio data*/
+    int32_t data[MAX_NUM_MICS][1<<FRAME_SIZE_LOG2];/**< Raw audio data*/
     s_metadata metadata[2];
 } frame_audio;
 
