52338c39057d4b204553b8d4d1c885a98f18f03d
Sam Chesney <samc@xmos.com>
2016-01-20 19:17:26 +0000

    Change backend test (fir model) to run on hardware

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -3,7 +3,12 @@ import xmostest
 
 def do_backend_test(frame_count, testlevel):
 
-    resources = xmostest.request_resource("xsim")
+    # Check if the test is running in an environment with hardware resources
+    # available - the test takes a long time to run in the simulator
+    args = xmostest.getargs()
+    if not args.remote_resourcer:
+        # Abort the test
+        return
 
     binary = 'test_fir_model/bin/COUNT{fc}/test_fir_model_COUNT{fc}.xe'.format(fc=frame_count)
 
@@ -15,11 +20,15 @@ def do_backend_test(frame_count, testlevel):
 
     tester.set_min_testlevel(testlevel)
 
-    xmostest.run_on_simulator(resources['xsim'], binary,
-                              simargs=[],
-                              tester = tester)
+    resources = xmostest.request_resource("uac2_xcore200_mc_testrig_os_x_11",
+                                          tester)
+
+    run_job = xmostest.run_on_xcore(resources['analysis_device_1'],
+                                    binary,
+                                    tester=tester)
+
+    xmostest.complete_all_jobs()
 
 def runtest():
     do_backend_test(4, "smoke")
     do_backend_test(64, "nightly")
-
