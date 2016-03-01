bb3469b25c3ac4bb8c1d7c66ba2eb56e8359f438
Andrew Stanford-Jason <andrew@xmos.com>
2016-03-01 09:29:54 +0000

    improved thd calc stript

--- tests/measure_tdh_plus_n/src/calc.py
+++ tests/measure_tdh_plus_n/src/calc.py
@@ -29,20 +29,32 @@ def THDN(signal, sample_rate, filename):
     signal = signal - mean(signal)  # this is so that the DC offset is not the peak in the case of PDM
     windowed = signal * blackmanharris(len(signal)) 
 
-    total_rms = rms_flat(windowed)
+    
 
     # Find the peak of the frequency spectrum (fundamental frequency), and filter 
     # the signal by throwing away values between the nearest local minima
     f = rfft(windowed)
+
+    #limit the bandwidth
+    if len(f) > 24000:
+       f[24000:len(f)] = 0
+
+    bandwidth_limited = irfft(f)
+    total_rms = rms_flat(bandwidth_limited)
+
+    #for i in range(6000):
+       #print abs(f[i])
     i = argmax(abs(f))
 
     #This will be exact if the frequency under test falls into the middle of an fft bin 
     print 'Frequency: %f Hz' % (sample_rate * (i / len(windowed)))
     lowermin, uppermin = find_range(abs(f), i)
+
+
+    #notch out the signal
     f[lowermin: uppermin] = 0
 
-    if len(f) > 24000:
-       f[24000:len(f)-1] = 0
+
 
     noise = irfft(f)
     THDN = rms_flat(noise) / total_rms
@@ -58,19 +70,25 @@ def load(filename):
         signals.append([signal, sample_rate, frequency_under_test])
         line_number.append(0)
 
+    data_dropped = 0
     for line in open(filename,'r').readlines():
         if line.startswith("<Record Type=\""):
             n = re.findall('-*\d+', line)
             chan = int(n[0])
             value = int(n[4])
 
-            if(line_number[chan] ==0):
-                signals[chan][1] = float(value)
-            elif line_number[chan] == 1:
-                signals[chan][2]  = float(value)
+            if chan < 16:
+                if(line_number[chan] ==0):
+                    signals[chan][1] = float(value)
+                elif line_number[chan] == 1:
+                    signals[chan][2]  = float(value)
+                else:
+                    signals[chan][0].append(float(value))
+                line_number[chan] += 1
             else:
-                signals[chan][0].append(float(value))
-            line_number[chan] += 1
+                data_dropped+=1
+    if data_dropped>0:
+        print "data dropped " + str(data_dropped)
     return signals
     
 files = sys.argv[1:]
@@ -82,7 +100,7 @@ if files:
             for s in signals:
                 if len(s[0])>0:
                     #verify that all the outputs happened
-                    print len(s[0])
+                    #print len(s[0])
                     THDN(s[0], s[1], filename)
         #except:
        #    print 'Couldn\'t analyze "' + filename + '"'
