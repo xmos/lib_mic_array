77d031f659aca12a95d1c4f04e1c013c0ffef256
Ed Clarke <ed@xmos.com>
2019-03-22 10:25:42 +0000

    Initial end to end THD test for mic_dual

---
+++ tests/test_mic_dual/test_all_thd.sh
@@ -0,0 +1,25 @@
+#Generate sines
+echo "Generating sine waves for test"
+sox -n -c 1 -b 32 -r 16000 ch_a_src.wav synth 30 sine 1000
+sox -n -c 1 -b 32 -r 16000 ch_b_src.wav synth 30 sine 3000
+
+
+#PCM to PDM
+python pcm_to_pdm.py --output-rate 3072000 --verbose ch_a_src.wav ch_a.pdm & \
+python pcm_to_pdm.py --output-rate 3072000 --verbose ch_b_src.wav ch_b.pdm
+
+#Run simulation
+echo "Running firmware"
+axe bin/test_mic_dual.xe
+
+#Add wav headers onto output PCM binaries
+sox --endian little -c 1 -r 16000 -b 32 -e signed-integer ch_a.raw ch_a.wav
+sox --endian little -c 1 -r 16000 -b 32 -e signed-integer ch_b.raw ch_b.wav
+
+#Do the THD calc
+python thdncalculator.py ch_a.wav & python thdncalculator.py ch_b.wav
+
+#Remove tmp files
+rm *.wav
+rm *.pdm
+rm *.raw
