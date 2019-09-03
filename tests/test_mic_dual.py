455854ed69318597d296af7a34329fd2103ac000
Ed <ed@xmos.com>
2019-09-03 15:26:42 +0100

    Add amplitude w.r.t. mic_array test (currently failing)

--- tests/test_mic_dual.py
+++ tests/test_mic_dual.py
@@ -10,7 +10,8 @@ from multiprocessing import Process
 
 test_dir = "test_mic_dual/"
 target_snr = -102.0 # SNR in dB. We be getting -120.7 or nearest offer
-AUDIO_LENGTH_S = 4 #Have found we need a good 4s to get a fair result
+target_ratio = 0.95 # Ratio between signal amplitudes of normal and dual mic_array
+AUDIO_LENGTH_S = 1.0 #4 #Have found we need a good 4s to get a fair result
 
 
 class THD_N_Tester(xmostest.Tester):
@@ -43,11 +44,49 @@ class THD_N_Tester(xmostest.Tester):
         xmostest.set_test_result(self.product, self.group, self.test, self.config, self.result,
                              output = output, env = self.env)
 
+class rms_tester(xmostest.Tester):
+    def __init__(self, max_variance, product, group, test, config = {}, env = {}):
+        super(rms_tester, self).__init__()
+        self.max_variance = max_variance
+        self.register_test(product, group, test, config)
+        self.product = product
+        self.group = group
+        self.test = test
+        self.config = config
+        self.env = env
+        self.result = True
+
+    def record_failure(self, failure_reason):
+        # Append a newline if there isn't one already
+        if not failure_reason.endswith('\n'):
+            failure_reason += '\n'
+        self.failures.append(failure_reason)
+        print ("ERROR: %s" % failure_reason), # Print without newline
+        self.result = False
+
+    def run(self, output):
+        out_files = ["ch_a_std.wav", "ch_b_std.wav", "ch_a_dual.wav", "ch_b_dual.wav"]
+        add_wav_headers(out_files)
+        results = [analyze_channels(test_dir + test_file, rms_flat) for test_file in out_files]
+        ch_a_dual_std_ratio = results[0] / results[2]
+        ch_b_dual_std_ratio = results[1] / results[3]
+        ch_a_dual_std_ratio = 1 / ch_a_dual_std_ratio if ch_a_dual_std_ratio > 1 else ch_a_dual_std_ratio
+        ch_b_dual_std_ratio = 1 / ch_b_dual_std_ratio if ch_b_dual_std_ratio > 1 else ch_b_dual_std_ratio
+
+        if ch_a_dual_std_ratio < target_ratio or ch_b_dual_std_ratio < target_ratio:
+            self.record_failure("RMS amplitudes of ch_a {} & ch_b {} of dual too large compared with mic_array. Raw results: {}".format(ch_a_dual_std_ratio, ch_b_dual_std_ratio, results))
+        xmostest.set_test_result(self.product, self.group, self.test, self.config, self.result,
+                             output = output, env = self.env)
 
 def do_mic_dual_test(testlevel, checker):
     resources = xmostest.request_resource("axe")
 
-    tester = THD_N_Tester(target_snr, 'lib_mic_array', 'mic_dual', 'thd_noise_{}'.format(testlevel))
+    if checker == THDN:
+        tester = THD_N_Tester(target_snr, 'lib_mic_array', 'mic_dual', 'thd_noise_{}'.format(testlevel))
+    elif checker == rms_flat:
+        tester = rms_tester(target_snr, 'lib_mic_array', 'mic_dual', 'amplitude_{}'.format(testlevel))
+    else:
+        fail("No checker passed to do_mic_dual_test")
 
     tester.set_min_testlevel(testlevel)
 
@@ -69,6 +108,7 @@ def create_test_pdm_signals(sox_args):
         this_cmd = cmd.format(test_dir + file_name).split()
         this_cmd.append(str(AUDIO_LENGTH_S))
         this_cmd.extend(sox_arg)
+        print(" ".join(this_cmd))
         subprocess.call(this_cmd)
 
     # Run in parallel
@@ -99,12 +139,16 @@ def cleanup_files():
     tmp_audio_files = [i for i in files if i.endswith('.wav') or i.endswith('.raw') or i.endswith('.pdm')]
     for tmp_file in tmp_audio_files:
         del_this = test_dir+tmp_file
-        print(del_this)
         os.remove(del_this)
 
 def runtest():
     create_test_pdm_signals((["sine", "300"], ["sine", "1000"]))
     do_mic_dual_test("smoke", THDN)
+
+    # re-use the previous sine signals
+    do_mic_dual_test("smoke", rms_flat)
+
+
     cleanup_files()
 
 
