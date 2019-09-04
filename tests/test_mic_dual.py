01ce4a18ae2c6f52ac57765a6918f7227a83ea1a
Ed <ed@xmos.com>
2019-09-04 18:09:47 +0100

    Add spectral flatness test

--- tests/test_mic_dual.py
+++ tests/test_mic_dual.py
@@ -7,12 +7,18 @@ sys.path.append("test_mic_dual")
 from pcm_to_pdm import pcm_to_pdm
 from thdncalculator import analyze_channels, THDN, rms_flat
 from multiprocessing import Process
+import numpy as np
+from scipy.signal import welch
+import soundfile as sf
 
 test_dir = "test_mic_dual/"
-target_snr = -102.0 # SNR in dB. We be getting -120.7 or nearest offer
-target_ratio = 0.95 # Ratio between signal amplitudes of normal and dual mic_array
-AUDIO_LENGTH_S = 1.0 #4 #Have found we need a good 4s to get a fair result
+target_snr = -102.0     # SNR in dB. We be getting -120.7 or nearest offer
+target_ratio = 0.95     # Ratio between signal amplitudes of normal and dual mic_array
+AUDIO_LENGTH_S = 1.0    # Have found that 1s, 4s, 10s gets a fair result. Others may cut at non-zero cross
+closeness = 0.015       # Relative closeness between spectral responses of mic_array and dual
 
+#These are the names of the files that get spat out of the sim. 
+out_files = ["ch_a_std.wav", "ch_b_std.wav", "ch_a_dual.wav", "ch_b_dual.wav"]
 
 class THD_N_Tester(xmostest.Tester):
     def __init__(self, target_snr, product, group, test, config = {}, env = {}):
@@ -35,12 +41,48 @@ class THD_N_Tester(xmostest.Tester):
         self.result = False
 
     def run(self, output):
-        out_files = ["ch_a_std.wav", "ch_b_std.wav", "ch_a_dual.wav", "ch_b_dual.wav"]
         add_wav_headers(out_files)
         results = [analyze_channels(test_dir + test_file, THDN) for test_file in out_files]
         for result, out_file in zip(results, out_files):
             if result > self.target_snr:
-                self.record_failure(out_file + " SNR is {}dB which is worse than target of {}dB".format(result, self.target_snr))
+                self.record_failure(out_file + "SNR is {}dB which is worse than target of {}dB".format(result, self.target_snr))
+        xmostest.set_test_result(self.product, self.group, self.test, self.config, self.result,
+                             output = output, env = self.env)
+
+class Spectral_Tester(xmostest.Tester):
+    def __init__(self, closeness, product, group, test, config = {}, env = {}):
+        super(Spectral_Tester, self).__init__()
+        self.closeness = closeness
+        self.register_test(product, group, test, config)
+        self.product = product
+        self.group = group
+        self.test = test
+        self.config = config
+        self.env = env
+        self.result = True
+
+    def get_fft(self, wav_file):
+        audio_data, fs = sf.read(test_dir+wav_file)
+        f, Pxx_spec = welch(audio_data, fs, 'flattop', 512, scaling='spectrum')
+        # Ditch anything above 7.0kHz or below 100Hz
+        idxs = np.where(f < 7000)
+        idxs = np.where(f[idxs] > 100)
+        return f[idxs], np.sqrt(Pxx_spec[idxs])
+
+    def record_failure(self, failure_reason):
+        # Append a newline if there isn't one already
+        if not failure_reason.endswith('\n'):
+            failure_reason += '\n'
+        self.failures.append(failure_reason)
+        print ("ERROR: %s" % failure_reason), # Print without newline
+        self.result = False
+
+    def run(self, output):
+        add_wav_headers(out_files)
+        for i in range(2):     
+            result = np.isclose(self.get_fft(out_files[i]), self.get_fft(out_files[i+2]), rtol=self.closeness)
+            if not result.all():
+                self.record_failure("Spectral response deviates between {} and {}: \n{}".format(out_files[i], out_files[i+2], result))
         xmostest.set_test_result(self.product, self.group, self.test, self.config, self.result,
                              output = output, env = self.env)
 
@@ -65,7 +107,6 @@ class rms_tester(xmostest.Tester):
         self.result = False
 
     def run(self, output):
-        out_files = ["ch_a_std.wav", "ch_b_std.wav", "ch_a_dual.wav", "ch_b_dual.wav"]
         add_wav_headers(out_files)
         results = [analyze_channels(test_dir + test_file, rms_flat) for test_file in out_files]
         ch_a_dual_std_ratio = results[0] / results[2]
@@ -85,6 +126,8 @@ def do_mic_dual_test(testlevel, checker):
         tester = THD_N_Tester(target_snr, 'lib_mic_array', 'mic_dual', 'thd_noise_{}'.format(testlevel))
     elif checker == rms_flat:
         tester = rms_tester(target_snr, 'lib_mic_array', 'mic_dual', 'amplitude_{}'.format(testlevel))
+    elif checker == Spectral_Tester.get_fft:
+        tester = Spectral_Tester(closeness, 'lib_mic_array', 'mic_dual', 'spectral_{}'.format(testlevel))
     else:
         fail("No checker passed to do_mic_dual_test")
 
@@ -148,7 +191,9 @@ def runtest():
     # re-use the previous sine signals
     do_mic_dual_test("smoke", rms_flat)
 
-
+    create_test_pdm_signals((["whitenoise"], ["white"]))
+    do_mic_dual_test("smoke", Spectral_Tester.get_fft)
+    
     cleanup_files()
 
 
