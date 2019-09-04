78586acaf1df6e917828f06c803bd2b54bfce215
Ed <ed@xmos.com>
2019-09-04 16:55:43 +0100

    Add timing test with xmostest script

--- tests/test_mic_dual_timing/src/mic_array_conf.h
+++ tests/test_mic_dual_timing/src/mic_array_conf.h
@@ -10,6 +10,8 @@
 #define MIC_FRAME_BUFFERS  2                // Don't care - Defined within mic_dual.xc
 #define MIC_CHANNELS 4                      // Don't care for mic_dual, always 2
 #define MIC_DECIMATORS 1                    // Don't care for mic_dual
-#define MIC_ARRAY_FRAME_SIZE (1)            // We *do* care about this in mic_dual
+#ifndef MIC_ARRAY_FRAME_SIZE
+#define MIC_ARRAY_FRAME_SIZE 240            // We *do* care about this in mic_dual
+#endif
 
 #endif /* MIC_ARRAY_CONF_H_ */
