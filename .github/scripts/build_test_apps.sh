a6d9c0145c6e7d86919ca27f383b0a44d222ed51
Ed <ed@xmos.com>
2023-07-28 09:49:32 +0100

    Try removing parallel build

--- .github/scripts/build_test_apps.sh
+++ .github/scripts/build_test_apps.sh
@@ -7,7 +7,7 @@ wget https://raw.githubusercontent.com/xmos/xmos_cmake_toolchain/main/xs3a.cmake
 wget https://raw.githubusercontent.com/xmos/xmos_cmake_toolchain/main/xc_override.cmake
 cmake -B build.xcore -DDEV_LIB_MIC_ARRAY=1 -DCMAKE_TOOLCHAIN_FILE=./xs3a.cmake
 pushd build.xcore
-make all -j
+make all
 popd
 # I can't get the following two (custom) targets to build with 'all'.
 # Currently an issue with java prevents these from working in CI.
