ac8adfa17be8f9b821c59f0d3c67d9f2afe9f8b2
lucianomartin <lucianom@xmos.com>
2024-02-21 16:05:14 +0000

    Pin lib_dsp

--- lib_mic_array/lib_build_info.cmake
+++ lib_mic_array/lib_build_info.cmake
@@ -8,6 +8,6 @@ set(LIB_C_SRCS "")
 
 set(LIB_DEPENDENT_MODULES "lib_xassert(4.2.0)"
                           "lib_logging(3.2.0)"
-                          "lib_dsp")
+                          "lib_dsp(6.3.0)")
 
 XMOS_REGISTER_MODULE()
