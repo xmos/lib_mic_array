cb3a10587c27073abb0eb383deb6a761ed83eaba
xalbertoisorna” <albertoisorna@xmos.com>
2026-02-11 16:42:43 +0000

    Update dependent module version in lib_build_info.cmake to use 'develop' branch

--- lib_mic_array/lib_build_info.cmake
+++ lib_mic_array/lib_build_info.cmake
@@ -1,6 +1,6 @@
 set(LIB_NAME lib_mic_array)
 set(LIB_VERSION 6.0.0)
-set(LIB_DEPENDENT_MODULES "lib_xcore_math(2.4.0)")
+set(LIB_DEPENDENT_MODULES "lib_xcore_math(develop)") #TODO pin version
 set(LIB_INCLUDES
     api
     api/mic_array
