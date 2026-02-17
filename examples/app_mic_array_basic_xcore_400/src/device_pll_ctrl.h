14c49a680e19a8436f9ed1ac4e4380bcd032fa65
Michael Banther <michaelb@xmos.com>
2026-02-17 14:02:10 +0000

    Make a copy of the basic mic array example for porting to xCore-400

---
+++ examples/app_mic_array_basic_xcore_400/src/device_pll_ctrl.h
@@ -0,0 +1,9 @@
+// Copyright 2022-2026 XMOS LIMITED.
+// This Software is subject to the terms of the XMOS Public Licence: Version 1.
+
+#pragma once
+
+#define DEVICE_PLL_CTL_VAL   0x0A019803 // Valid for all fractional values
+#define DEVICE_PLL_FRAC_NOM  0x800095F9 // 24.576000 MHz
+
+void device_pll_init(void);
