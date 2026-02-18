a9f10ecdf5fb27dda7e0b44b8d6dfeaa18489451
xalbertoisorna” <albertoisorna@xmos.com>
2026-02-18 17:08:52 +0000

    Add copyright notice and license terms to app_config.h

--- examples/app_mic_array_basic/src/app_config.h
+++ examples/app_mic_array_basic/src/app_config.h
@@ -1,3 +1,6 @@
+// Copyright 2026 XMOS LIMITED.
+// This Software is subject to the terms of the XMOS Public Licence: Version 1.
+
 #pragma once
 
 // -------------------- Frecuency and Port definitions --------------------
