e84ccd867d78eafc3b8edb850bdd315cd3188093
xalbertoisorna” <albertoisorna@xmos.com>
2026-02-23 10:12:40 +0000

    Fixing typo in __XS2A__

--- lib_mic_array/src/mic_array_task.cpp
+++ lib_mic_array/src/mic_array_task.cpp
@@ -19,7 +19,7 @@ bool use_3_stg_decimator = false;
 // until mic_array_start() completes. mic_array_start() performs shutdown and
 // then sets g_mics back to nullptr.
 
-#if !defined(XS2A)
+#if !defined(__XS2A__)
 ////////////////////
 // Mic array init //
 ////////////////////
@@ -178,4 +178,4 @@ extern "C" void _mic_array_override_pdm_port_c(chanend_t c_pdm)
   _mic_array_override_pdm_port(c_pdm);
 }
 
-#endif // !defined(XS2A)
+#endif // !defined(__XS2A__)
