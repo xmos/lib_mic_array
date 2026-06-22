6c3bcc96eb808236dbef6c360062e917305b6f00
xalbertoisorna” <albertoisorna@xmos.com>
2026-06-22 09:21:49 +0100

    fixing mic array conf.h

--- lib_mic_array/api/mic_array/mic_array_conf_full.h
+++ lib_mic_array/api/mic_array/mic_array_conf_full.h
@@ -1,13 +1,10 @@
 // Copyright 2015-2026 XMOS LIMITED.
 // This Software is subject to the terms of the XMOS Public Licence: Version 1.
 
-#ifndef _MIC_ARRAY_CONF_FULL_H_
-#define _MIC_ARRAY_CONF_FULL_H_
+#pragma once
 
 #ifdef __mic_array_conf_h_exists__
     #include "mic_array_conf.h"
-#endif
-
-#include "mic_array_conf_default.h"
-
+#else
+    #include "mic_array_conf_default.h"
 #endif
